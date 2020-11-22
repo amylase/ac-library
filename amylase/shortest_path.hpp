a90c55f335ea6cfd13d226e38307864eea04508b
amylase <amylase.inquiry@gmail.com>
2020-11-22 22:41:03 +0900

    Implemented 01BFS

--- amylase/shortest_path.hpp
+++ amylase/shortest_path.hpp
@@ -5,22 +5,28 @@
 #include <vector>
 #include <map>
 #include <queue>
+#include <limits>
 
 namespace amylase {
 
 template <class CostType>
-struct shortest_path {
+struct dijkstra {
     struct edge { int to; CostType cost; };
 
     int v;
     std::vector<std::vector<edge> > g;
-    shortest_path(int _v): v(_v), g(v) {}
+    dijkstra(int _v): v(_v), g(v) {}
 
     void add_edge (int from, int to, CostType cost) {
         g[from].push_back((edge){to, cost});
     }
-    std::map<int, CostType> get_distances(int init) {
-        std::map<int, CostType> min_cost;
+
+    static CostType inf() {
+        return std::numeric_limits<CostType>::max();
+    }
+
+    std::vector<CostType> get_distances(int init) {
+        std::vector<CostType> min_cost(v, inf());
         using queue_element = std::pair<CostType, int>;
         std::priority_queue<queue_element, std::vector<queue_element>, std::greater<queue_element>> q;
         min_cost[init] = 0;
@@ -37,7 +43,7 @@ struct shortest_path {
             for (int i = 0; i < g[pos].size(); ++i) {
                 int npos = g[pos][i].to;
                 CostType ncost = cost + g[pos][i].cost;
-                if (min_cost.find(npos) == min_cost.end() || ncost < min_cost[npos]) {
+                if (ncost < min_cost[npos]) {
                     min_cost[npos] = ncost;
                     q.emplace(ncost, npos);
                 }
@@ -47,6 +53,67 @@ struct shortest_path {
     }
 
     CostType get_distance(int s, int t) {
+        return get_distances(s)[t];
+    }
+};
+
+struct zero_one_bfs {
+    struct edge { int to; int cost; };
+
+    int v;
+    std::vector<std::vector<edge> > g;
+    zero_one_bfs(int _v): v(_v), g(v) {}
+
+    void add_zero_edge(int from, int to) {
+        add_edge(from, to, 0);
+    }
+
+    void add_one_edge(int from, int to) {
+        add_edge(from, to, 1);
+    }
+
+    void add_edge(int from, int to, int cost) {
+        assert(cost == 0 || cost == 1);
+        g[from].push_back((edge){to, cost});
+    }
+
+    static int inf() {
+        return std::numeric_limits<int>::max();
+    }
+
+    std::vector<int> get_distances(int init) {
+        std::vector<int> min_cost(v, inf());
+        using queue_element = std::pair<int, int>;
+        std::deque<queue_element> q;
+        min_cost[init] = 0;
+        q.emplace_front(0, init);
+        while (not q.empty()) {
+            int cost = q.front().first;
+            int pos = q.front().second;
+            q.pop_front();
+
+            if (cost > min_cost[pos]) {
+                continue;
+            }
+
+            for (int i = 0; i < g[pos].size(); ++i) {
+                int edge_cost = g[pos][i].cost;
+                int npos = g[pos][i].to;
+                int ncost = cost + edge_cost;
+                if (ncost < min_cost[npos]) {
+                    min_cost[npos] = ncost;
+                    if (edge_cost == 0) {
+                        q.emplace_front(ncost, npos);
+                    } else {
+                        q.emplace_back(ncost, npos);
+                    }
+                }
+            }
+        }
+        return min_cost;
+    }
+
+    int get_distance(int s, int t) {
         return get_distances(s)[t];
     }
 };
